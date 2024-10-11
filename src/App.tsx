6417db5140f7fe1b45e4044757e5f1cfb3bf0b9c
Mamedbekov Ruslan <mamedbekovruslan@yandex.ru>
2024-10-11 22:53:16 +0300

    add basic auth form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Box, TextField } from '@mui/material';
 import { FC } from 'react';
 
-const App: FC = () => 'test';
+const App: FC = () => (
+  <Box
+    component="form"
+    sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+    noValidate
+    autoComplete="off"
+  >
+    <TextField id="standard-basic" label="Логин" variant="standard" />
+    <TextField id="standard-basic" label="Пароль" variant="standard" />
+  </Box>
+);
 
 export default App;
