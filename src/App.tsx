72c917fb6df5b240d3042dc327e26c7d69f4a28f
Mamedbekov Ruslan <mamedbekovruslan@yandex.ru>
2024-10-11 21:59:41 +0300

    delete test type

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,6 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
-  const a = 12; // Ожидаем, что будет предупреждение
-  const b: number = 12; // Здесь предупреждений не будет, так как тип указан явно.
-
   return (
     <>
       <div>
