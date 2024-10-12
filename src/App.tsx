9ae8530b615158bb0b37d49c4b4f42b72560ff47
Mamedbekov Ruslan <mamedbekovruslan@yandex.ru>
2024-10-12 22:41:59 +0300

    make basic Auth component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
-import { Box, TextField } from '@mui/material';
+import './index.module.scss';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import { FC } from 'react';
 
 const App: FC = () => (
   <Box
-    component="form"
-    sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-    noValidate
-    autoComplete="off"
+    component="div"
+    sx={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#518ab1',
+    }}
   >
-    <TextField id="standard-basic" label="Логин" variant="standard" />
-    <TextField id="standard-basic" label="Пароль" variant="standard" />
+    <Box
+      component="form"
+      noValidate
+      autoComplete="off"
+      sx={{
+        width: '20%',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: 5,
+        backgroundColor: 'white',
+        borderRadius: 5,
+      }}
+    >
+      <Typography variant="h3" textAlign="center" color="#518ab1">
+        TRYGRAM
+      </Typography>
+      <TextField
+        id="standard-basic"
+        label="Логин"
+        variant="standard"
+        sx={{ margin: '10px 0' }}
+      />
+      <TextField
+        id="standard-basic"
+        label="Пароль"
+        variant="standard"
+        sx={{ margin: '10px 0' }}
+      />
+      <Button variant="contained" sx={{ marginTop: '45px', backgroundColor: '#518ab1' }}>
+        Вход
+      </Button>
+    </Box>
   </Box>
 );
 
