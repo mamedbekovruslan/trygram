7556ed5648b28a748a71e23991581fcf4ca6126d
Mamedbekov Ruslan <mamedbekovruslan@yandex.ru>
2024-10-16 09:46:10 +0300

    add react-router-dom + type main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom';
+
+const routes: RouteObject[] = [
+  {
+    path: '/auth',
+    element: <App />,
+  },
+];
+
+const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
